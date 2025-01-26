608eccf0d9f4d0fbf878f7c229ac074135e8ccce
Mobashir <mobashir49@outlook.com>
2025-01-26 13:48:11 +0530

    importing a method in PVM

---
+++ import/func.py
@@ -0,0 +1,5 @@
+# here we'll first define a function to add two numbers and prints the sum
+def sum(x, y):
+    print(x+y)
+
+sum(23, 67)
---
+++ import/import_func.py
@@ -0,0 +1,8 @@
+# Here we'll import the sum(x, y) defined inn func.py
+
+from func import sum
+
+a = 53
+b = 79
+
+sum(a, b)
